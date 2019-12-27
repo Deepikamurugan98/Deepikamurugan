dbff4cd32fa7d8174119e1907e10bbc3e5bba12d
ELCOT <ELCOT@Lenovo>
2019-12-27 12:31:45 +0530

    cls

--- appClientModule/java/Addition.java
+++ appClientModule/java/Addition.java
@@ -1,5 +1,16 @@
 package java;
 
 public class Addition {
-
+	private int a;
+	private int b;
+	public Addition() {
+		
+	}
+	public Addition(int a, int b) {
+		super();
+		this.a = a;
+		this.b = b;
+		System.out.println(a+b);
+	}
+	
 }
--- appClientModule/java/Main.java
+++ appClientModule/java/Main.java
@@ -1,5 +1,19 @@
 package java;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 public class Main {
+	public static void main(String ergs[])throws IOException{
+		int number1,number2;
+		InputStreamReader isr= new InputStreamReader(System.in);
+		BufferedReader br=new BufferedReader(isr);
+		number1=Integer.parseInt(br.readLine());
+	    number2=Integer.parseInt(br.readLine());
+		Addition add=new Addition();
+		Addition add1=new Addition();
+	}
+	
+	
+	
 
 }
